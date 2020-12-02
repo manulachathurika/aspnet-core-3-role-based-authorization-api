40f9412dc269d25cdbcdb5668211f1f14da6888d
Manula Thantriwatte <manulachathurika@gmail.com>
2020-12-02 09:22:09 +0530

    Add EquityController.cs class

---
+++ Controllers/EquityController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using WebApi.Services;
+using WebApi.Entities;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class EquityController : ControllerBase
+    {
+        public EquityController()
+        {
+            
+        }
+
+        [HttpGet]
+        [Route("getWelcome")]
+        [Authorize(Roles = Role.Admin)]
+        public string GetAll()
+        {
+            return "Welcome to equity";
+        }
+                
+    }
+}
