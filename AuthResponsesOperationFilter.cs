a775571143877eaf45a3851aab5968ade3cf8e22
Manula Thantriwatte <manulachathurika@gmail.com>
2020-12-02 08:51:43 +0530

    Add Swagger UI

---
+++ AuthResponsesOperationFilter.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace WebApi
+{
+    public class AuthResponsesOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var authAttributes = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
+                .Union(context.MethodInfo.GetCustomAttributes(true))
+                .OfType<AuthorizeAttribute>();
+
+            if (authAttributes.Any())
+            {
+                var securityRequirement = new OpenApiSecurityRequirement()
+                {
+                   {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                };
+                operation.Security = new List<OpenApiSecurityRequirement> { securityRequirement };
+                operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
+                operation.Responses.Add("403", new OpenApiResponse { Description = "Forbidden " });
+                operation.Responses.Add("500", new OpenApiResponse { Description = "Internal Server Error " });
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,11 @@ using WebApi.Services;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.IO;
+
+
+using Microsoft.OpenApi.Models;
+
 
 namespace WebApi
 {
@@ -52,11 +57,48 @@ namespace WebApi
 
             // configure DI for application services
             services.AddScoped<IUserService, UserService>();
+
+            services.AddSwaggerGen(c =>
+            {
+
+
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "LYNEAR APIs",
+                    Description = "LYNEAR APIs",
+
+                });
+
+                c.OperationFilter<AuthResponsesOperationFilter>();
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+
+                };
+
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), Path.Combine(Directory.GetCurrentDirectory(), Path.Combine(Directory.GetCurrentDirectory(), "wwwroot"), "XML"), "API.xml");
+
+                c.IncludeXmlComments(filePath);
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                c.EnableAnnotations();
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
             app.UseRouting();
 
             // global cors policy
@@ -71,6 +113,14 @@ namespace WebApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(Configuration["Swagger:Path"], Configuration["Swagger:Title"]);
+                c.RoutePrefix = string.Empty;
+                c.InjectStylesheet("./swagger-ui/custom.css");
+
             });
         }
     }
